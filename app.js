3d7f8b0452074453044c7a8d8b4dffabe0cd6cb9
joat28 <prabhat.rao@iitg.ac.in>
2020-07-20 18:05:07 +0530

    Add readmore function button and custom route parameters

--- app.js
+++ app.js
@@ -4,12 +4,6 @@ const ejs = require("ejs");
 const _ = require("lodash");
 
 const app = express();
-
-
-
-
-
-
 const blogArray=[];
 
 
@@ -18,10 +12,17 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 
+
+    //===================== INDEX ROUTE =========================
+
 app.route("/")
     .get(function(req,res){
         res.render("index",{blogArray:blogArray});
     });
+
+
+
+    //===================== COMPOSE ROUTE ========================
    
 app.route("/compose")
     .get(function(req,res){
@@ -29,24 +30,70 @@ app.route("/compose")
     })
     .post(function(req,res){
        // console.log(req.body);
-        
-        blogArray.push(req.body);
+        const blog={
+            id:blogArray.length,
+            blogTitle:req.body.blogTitle,
+            blogContent:req.body.blogContent,
+        }
+        blogArray.push(blog);
         //console.log(blogArray);
         res.redirect("/");
     });
-app.route("/edit")
+
+
+
+
+     //===================== EDIT/READ ROUTE ===========================
+
+
+app.route("/readpost/:blog_Id")
     .get(function(req,res){
-        res.render("edit");
+
+        var blogID=req.params.blog_Id;
+        var blogToRender;
+        for(var i=0;i<blogArray.length;++i){
+            if(blogArray[i].id == blogID){
+                blogToRender = blogArray[i];
+                console.log("found!!!!!!");
+                break;
+            }
+
+            //console.log(blogArray[i]);
+        }
+        //console.log(blogToRender);
+        res.render("readpost",{blogToRender:blogToRender});
+        
     });
+
+
+    // str = str.replace(/(?:\r\n|\r|\n)/g, '<br>');
+
+
+
+
+     //====================== ABOUT ROUTE ==========================
+
 
 app.route("/about")
     .get(function(req,res){
         res.render("about");
     });
+
+
+     //====================== CONTACT ROUTE==========================
+
+
+
 app.route("/contact")
     .get(function(req,res){
         res.render("contact");
     });
+
+
+
+
+
+     //====================== LISTEN TO PORT 3000 ====================
 
 app.listen(3000,function(req,res){
     console.log("Server started on port 3000!");
